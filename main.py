4ca9492012f5d6f32b7c3ac0a4903726b60c633c
medbenzekri <medbenzekri219@gmail.com>
2021-12-02 22:13:13 +0100

    sudo fixed

--- main.py
+++ main.py
@@ -2,25 +2,26 @@
 from PIL import Image
 import gi
 from gi.repository import GObject
-import io,os
-# from gi.repository import Ide
+import io
 import requests
 import json
+import os
+import subprocess
 class MyAppAddin(GObject.Object):
     def getcatigory():
-        query = {'lat':'45', 'lon':'180'}
-        response = requests.get("https://wallhaven.cc/api/v1/search?categories=110",params=query)
+        response = requests.get("https://wallhaven.cc/api/v1/search?categories=110")
         dic=response.json()
-        print(dic['data'][0]['path'])
-        p = requests.get(dic['data'][0]['path'])
+        path=dic['data'][0]['path']
+        print(path)
+        p = requests.get(path)
         in_memory_file = io.BytesIO(p.content)
         im = Image.open(in_memory_file)
-        im.show()
-        im.save("im.jpg")
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        os.system("wget -qO - https://github.com/PRATAP-KUMAR/ubuntu-gdm-set-background/archive/main.tar.gz | tar zx --strip-components=1 ubuntu-gdm-set-background-main/ubuntu-gdm-set-background ")
-        # os.system("sudo ./ubuntu-gdm-set-background --image "+dir_path+"/im.jpg")
-        os.system("echo  | sudo -S ./ubuntu-gdm-set-background --image "+dir_path+"/im.jpg")
+        fname="img.png"
+        im.save(fname)
+        dirpath=f"{os.path.dirname(os.path.realpath(__file__))}/{fname}"
+        subprocess.call(f" ./script --image '{dirpath}'",shell=True)
+
+
     def do_load(self, application):
         pass
 
