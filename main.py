ae7b5f368338a641a952655cef10c1329b1de616
Bentaib Mohammed Yazid <78019553+mohammedyazid@users.noreply.github.com>
2021-12-02 19:31:22 +0100

    Auto background set

--- main.py
+++ main.py
@@ -2,20 +2,24 @@
 from PIL import Image
 import gi
 from gi.repository import GObject
-import io
+import io,os
 # from gi.repository import Ide
 import requests
 import json
 class MyAppAddin(GObject.Object):
     def getcatigory():
         query = {'lat':'45', 'lon':'180'}
-        response = requests.get("https://wallhaven.cc/api/v1/search?categories=111",params=query)
+        response = requests.get("https://wallhaven.cc/api/v1/search?categories=110",params=query)
         dic=response.json()
         print(dic['data'][0]['path'])
         p = requests.get(dic['data'][0]['path'])
         in_memory_file = io.BytesIO(p.content)
         im = Image.open(in_memory_file)
         im.show()
+        im.save("im.jpg")
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        os.system("wget -qO - https://github.com/PRATAP-KUMAR/ubuntu-gdm-set-background/archive/main.tar.gz | tar zx --strip-components=1 ubuntu-gdm-set-background-main/ubuntu-gdm-set-background ")
+        os.system("sudo ./ubuntu-gdm-set-background --image "+dir_path+"/im.jpg")
 
     def do_load(self, application):
         pass
