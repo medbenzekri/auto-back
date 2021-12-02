f672fdee899f9e2cde284d697389813469b34fac
Bentaib Mohammed Yazid <78019553+mohammedyazid@users.noreply.github.com>
2021-12-02 17:38:13 +0100

    bruh

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 # my_plugin.py
 
 import gi
-
 from gi.repository import GObject
-from gi.repository import Ide
+# from gi.repository import Ide
+import requests
 
-class MyAppAddin(GObject.Object, Ide.ApplicationAddin):
-
+class MyAppAddin(GObject.Object):
     def getcatigory():
-        pass
+        query = {'lat':'45', 'lon':'180'}
+        response = requests.get("https://wallhaven.cc/api/v1/search?categories=111",params=query)
+        print(response.json())
+        
 
     def do_load(self, application):
-        
+        pass
 
     def do_unload(self, application):
         print("goodbye")
+MyAppAddin.getcatigory()
